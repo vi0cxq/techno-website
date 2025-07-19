7a01c7734967c5aefc9a5db92624a4fc8532af54
vi0cxq <vi0cxq@gmail.com>
2025-07-20 00:01:41 +1300

    remove live preview

--- apps/storefront/vite.config.ts
+++ apps/storefront/vite.config.ts
@@ -2,7 +2,8 @@ import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { enhancedImages } from '@sveltejs/enhanced-img';
 import { defineConfig } from 'vite';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 export default defineConfig({
-	plugins: [enhancedImages(), tailwindcss(), sveltekit()]
+	plugins: [enhancedImages(), tailwindcss(), sveltekit(), visualizer()]
 });
