be844c3effad2b63e1bed49c07de1246d9a96dfe
vi0cxq <vi0cxq@gmail.com>
2025-07-23 00:57:38 +1300

    cleanup transition animation

--- apps/storefront/src/routes/+layout.svelte
+++ apps/storefront/src/routes/+layout.svelte
@@ -6,24 +6,21 @@
 
 	import { gsap } from 'gsap';
 	import { ScrollTrigger } from 'gsap/ScrollTrigger';
-	import { ScrollToPlugin } from 'gsap/ScrollToPlugin';
+
+	import { CustomEase } from 'gsap/CustomEase';
 	import Lenis from 'lenis';
 
 	import Footer from '$lib/components/footer.svelte';
 	import Navigation from '$lib/components/navigation.svelte';
 
-	import { MediaQuery } from 'svelte/reactivity';
-	import type { Attachment } from 'svelte/attachments';
-	import { isFirstLoad } from '$lib/stores/navigation.svelte';
 	import '../app.css';
 
 	let { children, data }: { children: Snippet; data: { url: string } } = $props();
-	const desktop = new MediaQuery('min-width: 1024px');
 
 	let lenis: Lenis;
 
 	if (browser) {
-		gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
+		gsap.registerPlugin(ScrollTrigger, CustomEase);
 		lenis = new Lenis();
 		setContext<Lenis>('lenis', lenis);
 	}
@@ -57,30 +54,27 @@
 		}
 	});
 
-	const transition: Attachment = (element) => {
-		if (isFirstLoad()) {
-			// console.log('SKIP: First load');
-			return;
-		}
-
-		// console.log('RUN: Internal nav');
-
+	const slideIn = (element: Element, { delay, duration }: { delay?: number; duration: number }) => {
 		const tl = gsap.timeline();
 
 		tl.to(element, {
 			clipPath: 'inset(0% 0% 0% 0%)',
-			duration: desktop.current ? 1.3 : 1.6,
-			ease: 'expo.out'
-		});
-
-		tl.to(element, {
-			clipPath: 'inset(0% 0% 110% 0%)',
-			duration: desktop.current ? 1.3 : 1.6,
+			duration,
+			ease: 'expo.inOut'
+		}).to(element, {
+			clipPath: 'inset(0% 0% 100% 0%)',
+			duration,
 			ease: 'expo.out',
-			delay: desktop.current ? 0.3 : 0.4
+			delay: 0.3
 		});
 
-		return () => {};
+		return {
+			duration: duration * 1000,
+			delay,
+			tick: (t: number) => {
+				tl.progress(t);
+			}
+		};
 	};
 
 	function fade(node: Element, { delay, duration }: { delay?: number; duration: number }) {
@@ -101,14 +95,15 @@
 	{#key data.url}
 		<Navigation />
 
-		<div class="wrapper" {@attach transition}>
+		<div class="transition-wrapper" in:slideIn={{ duration: 2 }}>
 			<p class="text-foreground-2 text-base uppercase">
 				Techno <span>Ceram</span>
 			</p>
 		</div>
+
 		<div
 			class="bg-background relative z-10 mb-[100vh] flex min-h-screen flex-col"
-			out:fade={{ duration: desktop.current ? 1200 : 1300 }}
+			out:fade={{ duration: 800 }}
 		>
 			{@render children()}
 		</div>
@@ -117,14 +112,14 @@
 </QueryClientProvider>
 
 <style>
-	.wrapper {
+	.transition-wrapper {
 		position: fixed;
 		bottom: 0;
 		left: 0;
 		width: 100%;
-		height: 100svh;
+		height: 100%;
 		background: oklch(0.2191 0.0058 285.91);
-		clip-path: inset(110% 0% 0% 0%);
+		clip-path: inset(100% 0% 0% 0%);
 		z-index: 100000;
 		overflow: hidden;
 		display: flex;
